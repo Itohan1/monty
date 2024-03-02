a5be2a394ef130011da940e6593130b044a80876
Itohan1 <momoduitohan5@gmail.com>
2024-03-02 10:55:21 -0800

    correction

--- execute.c
+++ execute.c
@@ -13,17 +13,24 @@ int execute(char *value, stack_t **stack, unsigned int count, FILE *file)
 {
 	instruction_t son[] = {{"push", fpush}, {"pall", fpall}, {NULL, NULL}};
 
-	unsigned int i = 0;
+	unsigned int i = 0, limit = 0;
 	size_t c = 0;
 
 
 	char *op, *ops[2];
 
 	op = strtok(value, " \n\t");
+	
+	if (!op)
+		return (0);
 	while (op != NULL)
 	{
 		ops[c++] = op;
 		op = strtok(NULL, " \n\t");
+		limit++;
+
+		if (limit == 2)
+			break;
 	}
 	mine.arg = ops[0];
 	mine.arg2 = ops[1];
